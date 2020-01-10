08d5406113ab69ff1b63b7e7de242a588037d188
KrastevIT <barcodebit000@gmai.com>
2020-01-10 14:09:13 +0200

    update

---
+++ CoursesP2P.Models/Instructor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CoursesP2P.Models
+{
+    public class Instructor : User
+    {
+        public ICollection<Course> Courses { get; set; } = new HashSet<Course>();
+    }
+}
---
+++ CoursesP2P.Models/Student.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CoursesP2P.Models
+{
+    public class Student : User
+    {
+        public ICollection<StudentCourse> EnrolledCourses { get; set; } = new HashSet<StudentCourse>();
+    }
+}
