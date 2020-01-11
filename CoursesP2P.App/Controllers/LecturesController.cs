1f25acb902f69aa78bb5aed47e15f194cb4f7394
KrastevIT <barcodebit000@gmai.com>
2020-01-11 18:40:24 +0200

    update

---
+++ CoursesP2P.App/Controllers/LecturesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoursesP2P.App.Controllers
+{
+    public class LecturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
