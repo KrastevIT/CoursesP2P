b285ddfdbd5f168534c32afe191f3e66eb5e68a4
KrastevIT <barcodebit000@gmai.com>
2020-01-13 13:21:13 +0200

    update

--- CoursesP2P.App/Controllers/LecturesController.cs
+++ CoursesP2P.App/Controllers/LecturesController.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoursesP2P.App.Models.BindingModels;
+using CoursesP2P.Data;
+using CoursesP2P.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoursesP2P.App.Controllers
 {
     public class LecturesController : Controller
     {
+        private readonly CoursesP2PDbContext coursesP2PDbContext;
+
+        public LecturesController(CoursesP2PDbContext coursesP2PDbContext)
+        {
+            this.coursesP2PDbContext = coursesP2PDbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Add(int id)
+        {
+            ViewBag.id = id;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddLecturesBindingModel model, int id)
+        {
+            //TODO
+            var course = this.coursesP2PDbContext.Courses.FirstOrDefault(x => x.Id == id);
+
+            var lecture = new Lecture
+            {
+                Name = model.Name,
+                CourseId = id
+            };
+
+            course.Lectures.Add(lecture);
+            this.coursesP2PDbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Instructors");
         }
     }
 }
---
+++ CoursesP2P.App/Models/BindingModels/AddLecturesBindingModel.cs
@@ -0,0 +1,28 @@
+﻿using CoursesP2P.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesP2P.App.Models.BindingModels
+{
+    public class AddLecturesBindingModel
+    {
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Presentation { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+
+        [Required]
+        public IFormFile Video { get; set; }
+    }
+}
