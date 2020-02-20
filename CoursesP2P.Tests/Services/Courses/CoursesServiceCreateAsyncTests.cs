db3409e3680647cc7cfe77c9a691268d37cfd68d
KrastevIT <barcodebit000@gmai.com>
2020-02-20 13:28:45 +0200

    Refactoring CoursesTests

--- CoursesP2P.Tests/Services/Courses/CoursesServiceCreateAsyncTests.cs
+++ CoursesP2P.Tests/Services/Courses/CoursesServiceCreateAsyncTests.cs
@@ -1,42 +1,25 @@
 ﻿using AutoMapper;
 using CoursesP2P.Data;
 using CoursesP2P.Models;
-using CoursesP2P.Models.Enum;
 using CoursesP2P.Services.Courses;
 using CoursesP2P.Tests.Configuration;
-using CoursesP2P.ViewModels.Courses.BindingModels;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using Xunit;
 
 namespace CoursesP2P.Tests.Services.Courses
 {
-    public class CoursesServiceTests
+    public class CoursesServiceCreateAsyncTests
     {
         private CoursesP2PDbContext db;
-        private IMapper mapper;
+        private readonly IMapper mapper;
         private UserManager<User> userManager;
+        private CoursesService coursesService;
 
-        public CoursesServiceTests()
+        public CoursesServiceCreateAsyncTests()
         {
             this.db = new CoursesP2PDbContext(MemoryDatabase.OptionBuilder());
             this.mapper = MapperMock.AutoMapperMock();
             this.userManager = UserManagerMock.UserManagerMockTest();
-        }
-
-       
-
-       
-
-        
-
-        [Fact]
-        public void CreateCourse()
-        {
-
+            this.coursesService = new CoursesService(this.db, this.mapper, this.userManager);
         }
     }
 }
