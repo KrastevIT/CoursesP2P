2b187a562396dd66d0bbddaab91d35be3062e223
KrastevIT <barcodebit000@gmai.com>
2020-02-21 10:35:42 +0200

    Test InstructorsService

---
+++ CoursesP2P.Services/Instructors/IInstructorsService.cs
@@ -0,0 +1,12 @@
+﻿using CoursesP2P.Models;
+using CoursesP2P.ViewModels.Courses.ViewModels;
+
+namespace CoursesP2P.Services.Instructors
+{
+    public interface IInstructorsService
+    {
+        CourseAndDashbordViewModel GetCreatedCourses(User user);
+
+        void EditCourse(CourseEditViewModel model);
+    }
+}
---
+++ CoursesP2P.Tests/Services/Instructors/InstructorsServiceGetCreatedCoursesTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using CoursesP2P.Data;
+using CoursesP2P.Models;
+using CoursesP2P.Services.Instructors;
+using CoursesP2P.Tests.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CoursesP2P.Tests.Services.Instructors
+{
+    public class InstructorsServiceGetCreatedCoursesTests
+    {
+        private CoursesP2PDbContext db;
+        private IMapper mapper;
+        private InstructorsService instructorsService;
+
+        public InstructorsServiceGetCreatedCoursesTests()
+        {
+            this.db = new CoursesP2PDbContext(MemoryDatabase.OptionBuilder());
+            this.mapper = MapperMock.AutoMapperMock();
+            this.instructorsService = new InstructorsService(db,mapper);
+        }
+
+        [Fact]
+        public void GetCreatedCoursesReturnCorrectly()
+        {
+            var courses = new List<Course>
+            {
+                new Course
+                {
+                    Id = 1,
+                },
+                new Course
+                {
+                    Id = 2
+                }
+            };
+
+            var user = new User
+            {
+                Id = "1",
+                CreatedCourses = courses
+            };
+
+            this.db.Users.Add(user);
+            this.db.SaveChanges();
+
+
+            var actual = this.db.Users
+                .Include(x => x.CreatedCourses)
+                .Select(x => x.CreatedCourses.Count)
+                .Sum();
+
+            Assert.Equal(2, actual);
+        }
+    }
+}
