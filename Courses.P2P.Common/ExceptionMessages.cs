43463fd32fa8efa889047b872b2fc27d8126788f
KrastevIT <barcodebit000@gmai.com>
2020-04-30 14:54:32 +0300

    Edit ResetPassword

--- Courses.P2P.Common/ExceptionMessages.cs
+++ Courses.P2P.Common/ExceptionMessages.cs
@@ -3,9 +3,13 @@
     public class ExceptionMessages
     {
         public const string InvalidCourseId = "Невалиден курс {0}.";
+
         public const string InvalidLectureId = "Невалидна лекция {0}.";
+
         public const string NotFoundCourseById = "Курс {0} не е намерен";
+
         public const string InvalidCastCategory = "{0} Категорията не е намерена";
+
         public const string UnauthorizedUser = "Неупълномощен потребител {0}";
     }
 }
