42ceea8901b4d7c2490fb1139e6c2753d57c366c
KrastevIT <barcodebit000@gmai.com>
2020-04-30 21:15:50 +0300

    Add Category

--- CoursesP2P.App/ViewComponents/DropDownCategoriesViewComponent.cs
+++ CoursesP2P.App/ViewComponents/DropDownCategoriesViewComponent.cs
@@ -1,9 +1,8 @@
 ﻿using CoursesP2P.Models.Enum;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoursesP2P.App.ViewComponents
 {
@@ -17,9 +16,17 @@ namespace CoursesP2P.App.ViewComponents
             {
                 switch (category)
                 {
-                    case "Development": categories.Add(category, "fas fa-laptop-code"); break;
-                    case "Marketing": categories.Add(category, "fas fa-bullhorn"); break;
-
+                    case "Програмиране": categories.Add(category, "fas fa-laptop-code"); break;
+                    case "Маркетинг": categories.Add(category, "fas fa-bullhorn"); break;
+                    case "Бизнес": categories.Add(category, "far fa-chart-bar"); break;
+                    case "ИТ_и_Софтуер": categories.Add(category.Replace("_", " "), "fas fa-desktop"); break;
+                    case "Личностно_Развитие": categories.Add(category.Replace("_", " "), "fas fa-book-reader"); break;
+                    case "Дизайн": categories.Add(category, "fas fa-pencil-alt"); break;
+                    case "Начин_на_живот": categories.Add(category.Replace("_", " "), "fas fa-dove"); break;
+                    case "Фотография": categories.Add(category, "fas fa-camera-retro"); break;
+                    case "Здраве_и_фитнес": categories.Add(category.Replace("_", " "), "fas fa-heartbeat"); break;
+                    case "Музика": categories.Add(category, "fab fa-itunes-note"); break;
+                    case "Езици": categories.Add(category, "fas fa-language"); break;
                 }
             }
 
--- CoursesP2P.Models/Enum/Category.cs
+++ CoursesP2P.Models/Enum/Category.cs
@@ -1,8 +1,20 @@
-﻿namespace CoursesP2P.Models.Enum
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoursesP2P.Models.Enum
 {
     public enum Category
     {
-        Development = 0,
-        Marketing = 1
+        Програмиране = 0,
+        Маркетинг = 1,
+        Бизнес = 2,
+        ИТ_и_Софтуер = 3,
+        Личностно_Развитие = 4,
+        Дизайн = 5,
+        Начин_на_живот = 6,
+        Фотография = 7,
+        Здраве_и_фитнес = 8,
+        Музика = 9,
+        Езици = 10
     }
 }
