7696bb043b1c7feec5d269e79a27cc1d3477155e
KrastevIT <barcodebit000@gmai.com>
2020-05-10 11:22:25 +0300

    Edit JS Add Lecture

--- CoursesP2P.App/Controllers/LecturesController.cs
+++ CoursesP2P.App/Controllers/LecturesController.cs
@@ -60,6 +60,8 @@ namespace CoursesP2P.App.Controllers
                 return Json("invalid");
             }
 
+            return Json("valid");
+
             var inputAsset = await this.azureMediaService.CreateInputAssetAsync(model.Video);
 
             var outputAsset = await this.azureMediaService.CreateOutputAssetAsync();
